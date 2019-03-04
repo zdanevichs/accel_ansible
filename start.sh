11f3b9b56f7112c6f16a3d54e1ec3dfbf4f6ca74
vilko <helgi8@mail.ru>
2019-03-04 12:34:08 +0300

    roles

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt-get install git -y
-git clone https://github.com/mikbill/accel_ansible
-cd accel_ansible
-chmod +x ansible_install_deb.sh
-chmod +x install.sh
-./ansible_install_deb.sh
-./install.sh
