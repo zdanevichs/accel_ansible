c12971151809be021feca41ba1505f790a4b7213
vilko <helgi8@mail.ru>
2019-03-11 08:02:29 +0300

    mod

--- ansible_install_deb.sh
+++ ansible_install_deb.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-#echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >> /etc/apt/sources.list
+str="deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main"
+filename="/etc/apt/sources.list"
+if ! grep -w -q "$str" $filename; then
+ echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" >>$filename
+fi
+apt-get install dirmngr -y
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 apt-get update
-apt-get install ansible 
+apt-get install ansible -y --allow-unauthenticated
