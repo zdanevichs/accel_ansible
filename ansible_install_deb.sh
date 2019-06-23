ac90fbf589d7babae87300147f3de3ebbd38070c
Oleg Vilkovsky <helgi8@mail.ru>
2019-06-23 13:40:27 +0300

    Update ansible_install_deb.sh

--- ansible_install_deb.sh
+++ ansible_install_deb.sh
@@ -2,7 +2,7 @@
 str="deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main"
 filename="/etc/apt/sources.list"
 if ! grep -w -q "$str" $filename; then
- echo $str >>$filename
+  echo $str >>$filename
 fi
 apt-get install dirmngr -y
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
