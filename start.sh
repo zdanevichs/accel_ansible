ca020cfd1d909e48feb53780c7458905dd331c08
Sergei Zdanevich <sergei@zdanevich.ru>
2021-07-02 11:58:23 +0300

    Change repository for install

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get install git -y
-git clone https://github.com/mikbill/accel_ansible
+git clone https://github.com/zdanevichs/accel_ansible
 cd accel_ansible
 chmod +x ansible_install_deb.sh
 chmod +x install.sh
